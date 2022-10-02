c468dddb4a31cd126354c0c4c0f0aa6f82015811
zql0612 <114821171+zql0612@users.noreply.github.com>
2022-10-02 23:20:27 +0800

    pull commit

--- src/test/java/com/kuang/GitTest.java
+++ src/test/java/com/kuang/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fix2");
         System.out.println("hello push test");
+        System.out.println("hello pull test");
     }
 }
