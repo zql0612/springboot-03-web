dccd8ff320d00536d4458888f7c2afd80158e6c4
zqliang <38725223@qq.com>
2022-10-02 22:02:15 +0800

    push commit

--- src/test/java/com/kuang/GitTest.java
+++ src/test/java/com/kuang/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fix2");
+        System.out.println("hello push test");
     }
 }
